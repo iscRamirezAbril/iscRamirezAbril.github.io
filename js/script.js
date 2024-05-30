26c01874d6c13656dd815844077c194ea6fd57c6
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2024-05-30 19:15:28 -0400

    Finished Backend and new Palette Color

---
+++ js/script.js
@@ -0,0 +1,56 @@
+/* |===============| Toggle Icon Navbar |===============| */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+/* |===============| Scroll sections active link |===============| */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /* |===============| Sticky Navbar |===============| */
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100)
+
+    /* |===============| Remove toggle icon and navbar when click navbar link (scroll) |===============| */
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/* |===============| Scroll Reveal |===============| */
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading, .subtitle, form', { origin: 'top' });
+ScrollReveal().reveal('.profile-pic, .skills-row, .card, .title, .home-content h1', { origin: 'left' });
+ScrollReveal().reveal('.education-row, .home-content p', { origin: 'right' });
+
+/* |===============| Type Js |===============| */
+const typed = new Typed('.multiple-text', {
+    strings: ['UI/UX Designer', 'C# Developer', 'Frontend Developer','Project Manager'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
